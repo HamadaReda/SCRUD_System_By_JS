7e5f960be7269d6b97578972e5862d4c21776a77
Hamada Reda <enghamadareda444@gmail.com>
2024-07-09 22:55:12 +0300

    enhance the main file of JavaScript

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Element references
 const productName = document.getElementById("productName");
 const productCategory = document.getElementById("productCategory");
 const productPrice = document.getElementById("productPrice");
@@ -10,17 +11,88 @@ const updateButton = document.getElementById("updateButton");
 const searchInput = document.getElementById("searchInput");
 const scrud_form = document.getElementById("scrud_form");
 
+// Initial setup
+let allProducts = JSON.parse(localStorage.getItem("products")) || [];
+let tempIndex = null;
+
+// Event listeners
 scrud_form.addEventListener("submit", (event) => event.preventDefault());
+addButton.addEventListener("click", addProduct);
+updateButton.addEventListener("click", updateProduct);
+searchInput.addEventListener("input", filterProducts);
 
-let allProducts = [];
+// Display products on load
+displayProducts();
 
-if (localStorage.getItem("products")) {
-  allProducts = JSON.parse(localStorage.getItem("products"));
+// Functions
+function addProduct() {
+  const product = getProductFromInputs();
+  allProducts.push(product);
+  updateLocalStorage();
+  displayProducts();
+  clearInputs();
+}
+
+function updateProduct() {
+  const product = getProductFromInputs();
+  allProducts[tempIndex] = product;
+  updateLocalStorage();
+  displayProducts();
+  toggleButtons();
+  clearInputs();
+}
+
+function deleteProduct(productIndex) {
+  allProducts.splice(productIndex, 1);
+  updateLocalStorage();
   displayProducts();
 }
 
-function addProduct() {
-  let product = {
+function editProduct(productIndex) {
+  const product = allProducts[productIndex];
+  setInputsFromProduct(product);
+  tempIndex = productIndex;
+  toggleButtons();
+}
+
+function filterProducts(event) {
+  const nameText = event.target.value.toLowerCase();
+  const filteredProducts = allProducts.filter((product) =>
+    product.name.toLowerCase().includes(nameText)
+  );
+  displayProducts(filteredProducts);
+}
+
+function displayProducts(products = allProducts) {
+  const data = products
+    .map(
+      (product, index) => `
+    <tr>
+      <td class='ps-2'>${product.name}</td>
+      <td class='ps-2'>${product.category}</td>
+      <td class='ps-2'>${product.price}</td>
+      <td class='ps-2'>${product.discount}</td>
+      <td class='ps-2'>${product.quantity}</td>
+      <td class='ps-2'>${product.description}</td>
+      <td class='ps-2'>
+        <button class="btn btn-success" onclick="editProduct(${index})">
+          <i class="fa-solid fa-pen-to-square"></i>
+        </button>
+      </td>
+      <td class='ps-2'>
+        <button class="btn btn-danger" onclick="deleteProduct(${index})">
+          <i class="fa-solid fa-xmark"></i>
+        </button>
+      </td>
+    </tr>
+  `
+    )
+    .join("");
+  showProducts.innerHTML = data;
+}
+
+function getProductFromInputs() {
+  return {
     name: productName.value,
     category: productCategory.value,
     price: productPrice.value,
@@ -28,106 +100,26 @@ function addProduct() {
     quantity: productQuantity.value,
     description: productDescription.value,
   };
-  allProducts.push(product);
-  localStorage.setItem("products", JSON.stringify(allProducts));
-  displayProducts();
-  clearInputs();
 }
 
-function displayProducts() {
-  let data = ``;
-  for (let i = 0; i < allProducts.length; i++) {
-    data += `
-      <tr>
-        <td class='ps-2'>${allProducts[i].name}</td>
-        <td class='ps-2'>${allProducts[i].category}</td>
-        <td class='ps-2'>${allProducts[i].price}</td>
-        <td class='ps-2'>${allProducts[i].discount}</td>
-        <td class='ps-2'>${allProducts[i].quantity}</td>
-        <td class='ps-2'>${allProducts[i].description}</td>
-        <td class='ps-2'>
-          <button class="btn btn-success" onclick="editProduct(${i})">
-            <i class="fa-solid fa-pen-to-square"></i>
-          </button>
-        </td>
-        <td class='ps-2'>
-          <button class="btn btn-danger" onclick="deleteProduct(${i})">
-            <i class="fa-solid fa-xmark"></i>
-          </button>
-        </td>
-      </tr>
-    `;
-  }
-  showProducts.innerHTML = data;
-}
-
-function deleteProduct(productIndex) {
-  allProducts.splice(productIndex, 1);
-  localStorage.setItem("products", JSON.stringify(allProducts));
-  displayProducts();
-}
-
-let tempIndex;
-function editProduct(productIndex) {
-  let product = allProducts[productIndex];
+function setInputsFromProduct(product) {
   productName.value = product.name;
   productCategory.value = product.category;
   productPrice.value = product.price;
   productDiscount.value = product.discount;
   productQuantity.value = product.quantity;
   productDescription.value = product.description;
-  addButton.classList.add("d-none");
-  updateButton.classList.remove("d-none");
-  tempIndex = productIndex;
-}
-
-function updateProduct() {
-  let product = {
-    name: productName.value,
-    category: productCategory.value,
-    price: productPrice.value,
-    discount: productDiscount.value,
-    quantity: productQuantity.value,
-    description: productDescription.value,
-  };
-  allProducts.splice(tempIndex, 1, product);
-  localStorage.setItem("products", JSON.stringify(allProducts));
-  addButton.classList.remove("d-none");
-  updateButton.classList.add("d-none");
-  displayProducts();
-  clearInputs();
 }
 
 function clearInputs() {
   scrud_form.reset();
 }
 
-searchInput.addEventListener("input", function (event) {
-  let nameText = event.target.value;
-  let data = ``;
-  for (let i = 0; i < allProducts.length; i++) {
-    if (allProducts[i].name.toLowerCase().includes(nameText.toLowerCase())) {
-      data += `
-      <tr>
-        <td>${allProducts[i].name}</td>
-        <td class='ps-2'>${allProducts[i].category}</td>
-        <td class='ps-2'>${allProducts[i].price}</td>
-        <td class='ps-2'>${allProducts[i].discount}</td>
-        <td class='ps-2'>${allProducts[i].quantity}</td>
-        <td class='ps-2'>${allProducts[i].description}</td>
-        <td class='ps-2'>
-          <button class="btn btn-success" onclick="editProduct(${i})">
-            <i class="fa-solid fa-pen-to-square"></i>
-          </button>
-        </td>
-        <td class='ps-2'>
-          <button class="btn btn-danger" onclick="deleteProduct(${i})">
-            <i class="fa-solid fa-xmark"></i>
-          </button>
-        </td>
-      </tr>
-    `;
-    }
-  }
-  showProducts.innerHTML = data;
-});
+function toggleButtons() {
+  addButton.classList.toggle("d-none");
+  updateButton.classList.toggle("d-none");
+}
+
+function updateLocalStorage() {
+  localStorage.setItem("products", JSON.stringify(allProducts));
+}
